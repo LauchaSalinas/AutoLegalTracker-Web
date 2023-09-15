8bdf0761999886fd64853b8b40abfc1344c275ac
MarcJuan <gonzalez_juan_marcos@hotmail.com>
2023-09-14 23:03:08 -0300

    fixeando login

--- src/components/LandingPage/Login.jsx
+++ src/components/LandingPage/Login.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useEffect, useState } from 'react'
 import { useGoogleLogin } from '@react-oauth/google'
 import axios from 'axios';
 const host = import.meta.env.VITE_TOKEN_EXCHANGE_SERVER_URL;
@@ -7,8 +8,8 @@ function Login() {
 //#region Google Login
   const login = useGoogleLogin({
     onSuccess: tokenResponse => {
-        // send tokenResponse.code to your backend to get access token
-        exchangeCode(tokenResponse.code);
+      // send tokenResponse.code to your backend to get access token
+      exchangeCode(tokenResponse.code);
     },
     onFailure: error => console.log(error),
     // indicate google to use the auth-code flow for server side exchange
@@ -18,74 +19,122 @@ function Login() {
   });
 
   const exchangeCode = async (oneTimeToken) => {
-    console.log(oneTimeToken);
-    const tokens = await axios.post(
-      (host+"/User/login"), 
-        {  
-            oneTimeToken,
-        },
+    try {
+      console.log(oneTimeToken);
+      const tokens = await axios.post(
+        (host+"/User/login"), 
+          {  
+            oneTimeToken: oneTimeToken,
+          },
+          {
+            withCredentials: true
+          }
+      )
+      console.log(tokens);
+      setIsPending(true);
+      console.log(isPending)
+    } catch (error) {
+      console.log(error);
+    }
+  //#endregion
+  }
+  
+  //#region logout
+  const [userInfo, setUserInfo] = useState({
+    name: '',
+    imgUrl: '',
+  })
+
+  const [isPending, setIsPending] = useState(false); 
+
+  // use the react hook to get the user info
+  const getUserInfo = async () => {
+    try {
+      const response = await axios.get(
+        (host+"/User/Info"),
         {
           withCredentials: true
         }
-    );
+      );
 
-    console.log(tokens);
+      setUserInfo(x => ({
+        name: response.data.name,
+        imgUrl: response.data.imageUrl
+      }));
+    } catch (error) {
+      console.log(error);
+    }
   }
   //#endregion
 
-//#region logout
-const [userInfo, setUserInfo] = React.useState({
-  name: '',
-  imgUrl: '',
-})
+  //#region logout
+  const logout = async () => {
+    const response = await axios.post(
+      (host+"/User/logout"),
+      {},
+      {
+        withCredentials: true
+      }
+    );
+    setUserInfo({
+      name: '',
+      imgUrl: '',
+    })
+    
+    setIsPending(false);
+  }
 
-// use the react hook to get the user info
-const getUserInfo = async () => {
-  const response = await axios.get(
-    (host+"/User/Info"),
-    {
-      withCredentials: true
-    }
-  );
-  setUserInfo(x => ({
-    name: response.data.name,
-    imgUrl: response.data.imageUrl
-  }));
-  console.log(response);
-}
+  //estado que define si se muestra o no el desplegable de sesión
+  const [logoutVisible, setLogoutVisible] = useState(false);
+  
+  // metodo para cambiar el estado dependiendo del estado actual
+  const toggleState = () => {
+    setLogoutVisible((prevState) => !prevState);
+  };
+
+  useEffect(() => {
+    console.log(isPending);
+    if(isPending)
+      getUserInfo();
+  }, [isPending])
+
 //#endregion
-
-//#region logout
-const logout = async () => {
-  const response = await axios.post(
-    (host+"/User/logout"),
-    {},
-    {
-      withCredentials: true
-    }
-  );
-  console.log(response);
-}
-//#endregion
-
-return (
-  <div className='bg-white text-black font-semibold border-[3px] border-white rounded hover:bg-transparent hover:text-white duration-75' id='divLogin'>
-    <button 
+  return (
+    <div className='p-5 h-10 flex items-center text-sm bg-red-600 text-black font-semibold border-[3px] border-white rounded hover:text-white duration-75 ' id='divLogin'>
+    { userInfo.name == '' || userInfo.imgUrl == ''
+    ?
+      <button 
       className='p-1.5 text-sm'
       onClick={() => login()}
-    >
-      Sign in with Google 🚀{' '}
-    </button>
-    <button onClick={() => getUserInfo()}>
-      Get User info{' '}
-    </button>
-    <h2>{userInfo.name}</h2>
-    <img style={{margin: '15px'}} src={userInfo.imgUrl} />
-    <button onClick={() => logout()}>
-      Logout ❌{' '}
-    </button>
-  </div>
-)
+      >
+        Sign in with Google 🚀{' '}
+      </button>
+    :
+      <>
+      <div className='w-full h-full cursor-pointer flex flex-col justify-star items-center '>
+        {/* USER INFO */}
+        <div className='w-full h-full flex justify-between items-center' onClick={toggleState}>
+          <span className='text-xs font-normal'>{userInfo.name}</span>
+          <img className='w-8 rounded-full' src={userInfo.imgUrl} />  
+        </div>
+        <div>
+          {logoutVisible 
+          ?
+            <button 
+              onClick={() => logout()}
+              className='h-[80px] ml-5 text-xs font-normal'
+            >
+              Logout ❌{' '}
+            </button>
+          : null  
+          }
+          
+        </div>
+      </div>
+      </>
+    } 
+    </div>
+  )
+  
 }
-
 export default Login
